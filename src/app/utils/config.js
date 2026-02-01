ae815a91ac7821f3d6cb9ec53ab2bc640c15c5b2
Sadam Root <86251542+rootprotest@users.noreply.github.com>
2026-02-01 06:21:53 +0530

    last

--- src/app/utils/config.js
+++ src/app/utils/config.js
@@ -3,9 +3,9 @@ import { ENDPOINTS } from './endpoints';
 
 
 const DEBUG = true;
-// const BASE_URL = 'https://apis.washmonkey.in/';
+const BASE_URL = 'https://apis.washmonkey.in/';
 // const BASE_URL = 'http://192.168.88.33:4000/';
-const BASE_URL = 'http://192.168.31.238:4000/';
+// const BASE_URL = 'http://192.168.31.238:4000/';
 
 const AUTHORIZATION_TOKEN = 'aH3KCew1YsWhWqW0tqNU3ndzHb3RdblI';
 
