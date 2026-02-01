cde68e3b634bce64c894505b059025446f56e643
Sadam Hussain <90755325+sadamhussain4752@users.noreply.github.com>
2026-02-01 13:26:59 +0530

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -6,7 +6,7 @@ const DEBUG = true;
 const BASE_URL = 'https://apis.washmonkey.in/';
 // const BASE_URL = 'http://192.168.88.33:4000/';
 // const BASE_URL = 'http://192.168.31.238:4000/';
-
+//
 const AUTHORIZATION_TOKEN = 'aH3KCew1YsWhWqW0tqNU3ndzHb3RdblI';
 
 const CONFIG = {
